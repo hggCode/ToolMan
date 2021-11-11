82e792ed9a400cf48352ae433bd2c8d2dbebda37
500code <haizhia1@163.com>
2021-11-11 14:27:48 +0800

    添加了依赖

--- service/service_tm/src/main/java/work/wlwl/toolman/service/tm/ServiceTMApplication.java
+++ service/service_tm/src/main/java/work/wlwl/toolman/service/tm/ServiceTMApplication.java
@@ -1,4 +1,19 @@
 package work.wlwl.toolman.service.tm;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@ComponentScan({"work.wlwl.toolman"})
+
 public class ServiceTMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTMApplication.class, args);
+    }
 }
